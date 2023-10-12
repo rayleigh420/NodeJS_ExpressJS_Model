2c5908ec7fc8e092de5f19c08633e27e26f96729
rayleigh420 <nhatduy0409@gmail.com>
2023-10-12 10:08:11 +0700

    Connect mysql server

---
+++ src/config/connectDB.js
@@ -0,0 +1,19 @@
+import { Sequelize } from 'sequelize'
+require('dotenv').config()
+
+const sequelize = new Sequelize('web-advanced', 'root', null, {
+  host: 'localhost',
+  dialect: 'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+  logging: false
+});
+
+let connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('-------------Connected!! Connection has been established successfully-------------');
+    } catch (error) {
+        console.error('!!!!!!!!!Connect Database Failed!! Unable to connect to the database:', error, "!!!!!!!!!");
+    }
+}
+
+module.exports = connectDB;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express"
 import initRoutes from './routes/routes'
 require('dotenv').config()
 
+import connectDB from './config/connectDB'
+
 const app = express()
 const port = process.env.PORT || 8080
 
@@ -11,6 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 initRoutes(app)
+connectDB()
 
 app.listen(port, () => {
     console.log(`-------------Web is listening on port ${port}------------`)
